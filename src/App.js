02e640cb7b449d929e0856fc4c0700e9cdba3581
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-12 19:30:13 -0400

    commit tree 2402d38b9e572d739bd92b9fbf45b1c8da108c73

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
 // create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+    
+    constructor() {
+        super()
+    }
+    state = {
+        peopleInSpace: []
+    }
+    
+
+    compontentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({
+                peopleInSpace: data.people
+            })
+        })
+    }
+
+    render() {
+        return (
+          <div>
+            {this.state.peopleInSpace.map(person => person.name)}
+          </div>
+        )
+      }
+}
+
+export default App
